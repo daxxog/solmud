c8896c851cd2cc501e1c2f714c2072e5ea4b5ac2
David Volm <david@volminator.com>
2026-01-20 10:32:14 -0600

    cleanup

--- pkg/math/trig.go
+++
@@ -1,9 +0,0 @@
-package math
-
-func (t *TrigTable) Sin(index int) int {
-	return t.sin[index%2048]
-}
-
-func (t *TrigTable) Cos(index int) int {
-	return t.cos[index%2048]
-}
--- pkg/math/vec2.go
+++
@@ -1,9 +0,0 @@
-package math
-
-func (v *Vec2) X() int {
-	return v.x
-}
-
-func (v *Vec2) Y() int {
-	return v.y
-}
